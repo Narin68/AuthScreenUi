0e232b362f3b48afc6d2587e7fd4db9177697aa6
Narin68 <pachnarin68@gmail.com>
2022-07-11 22:52:00 +0700

    Initial Commit

---
+++ AuthScreenUI/AuthScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthScreenUIApp.swift
+//  AuthScreenUI
+//
+//  Created by Touch prakphou on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AuthScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
